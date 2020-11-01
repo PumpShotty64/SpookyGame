3b027c5164e803789cc544e23328d55d0699be17
Alexis Chavoya <chavoyaa@uci.edu>
2020-11-01 11:19:52 -0800

    dyeing sounds

--- SpookyFriends/Scripts/Enemy2Death.cs
+++ SpookyFriends/Scripts/Enemy2Death.cs
@@ -4,12 +4,16 @@ using System;
 public class Enemy2Death : Node2D
 {
 	AnimatedSprite animation = null;
+	private AudioStreamPlayer audio = null;
 	
 	public override void _Ready()
 	{
+		audio = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+		audio.VolumeDb = 4;
 		animation = GetNode<AnimatedSprite>("AnimatedSprite");
 		animation.Frame = 0;
 		GetNode("AnimatedSprite").Connect("animation_finished", this, nameof(_on_AnimatedSprite_animation_finished));
+		audio.Play();
 		animation.Play("anime");
 	}
 	
